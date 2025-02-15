2ae4b89631068e5c72620441e89629446e6f6f1b
Soporte Netec <154626442+NetecGk@users.noreply.github.com>
2025-02-14 18:02:50 -0600

    Delete Unidad1 directory

--- Unidad1/code/ms-clients/src/main/java/com/netec/app/repository/ClientRepository.java
+++
@@ -1,8 +0,0 @@
-package com.netec.app.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.netec.app.entities.Client;
-
-public interface ClientRepository extends JpaRepository<Client, Long> {
-}
